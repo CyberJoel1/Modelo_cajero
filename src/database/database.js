c0db40c09e738dd467c87cf15937c83c14109a81
CyberJoel1 <68552274+CyberJoel1@users.noreply.github.com>
2022-03-17 23:12:39 -0500

    Datos base de datos

--- src/database/database.js
+++ src/database/database.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('Cajero3'
-, 'postgres'
-, '18062014', {
-    host: 'localhost',
+const sequelize = new Sequelize('daoce6i88n8pda'
+, 'etercqlrndounz'
+, 'c4d61bf194bb4869f5b67c7c2086288c7532583e3907294343b60f25c046f373', {
+    host: 'ec2-52-44-209-165.compute-1.amazonaws.com',
     dialect: 'postgres'
   });
   
